dd3db5bed3ca02dd5787e3056d90768a55842d9f
Roman Lytvyn <lytvyn18@gmail.com>
2023-10-01 21:13:57 +0300

    resolve two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const step = Math.floor(Math.sqrt(breaks.length));
+    let breakingPoint = step;
 
+    for (; breaks.length; breakingPoint += step) {
+        if (breaks[breakingPoint]) {
+            break;
+        }
+    }
+
+    breakingPoint -= step;
+
+    for (let j = 0; j < step && breakingPoint < breaks.length; ++breakingPoint, ++j) {
+        if (breaks[breakingPoint]) {
+            return breakingPoint;
+        }
+    }
+
+    return -1;
 }
