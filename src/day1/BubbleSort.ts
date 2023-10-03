2a1fee65ef48750ff49e4346d2be20cea96708bd
Roman Lytvyn <lytvyn18@gmail.com>
2023-10-03 22:43:34 +0300

    add alternative bubble sort implementation

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -11,6 +11,24 @@ export default function bubble_sort(arr: number[]): void {
     }
 }
 
+// alternative variant (a bit more efficient)
+// export default function bubble_sort(arr: number[]): void {
+//     const n = arr.length;
+//     let swapped;
+//
+//     do {
+//         swapped = false;
+//         for (let i = 0; i < n - 1; i++) {
+//             if (arr[i] > arr[i + 1]) {
+//                 const temp = arr[i];
+//                 arr[i] = arr[i + 1];
+//                 arr[i + 1] = temp;
+//                 swapped = true; // Mark that a swap was made
+//             }
+//         }
+//     } while (swapped);
+// }
+
 // my implementation (less efficient)
 // export default function bubble_sort(arr: number[]): void {
 //     let idx = 0;
