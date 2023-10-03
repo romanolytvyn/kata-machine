cfc05c507806c72104b5b18a3e2826dac47f15ef
Roman Lytvyn <lytvyn18@gmail.com>
2023-10-03 22:35:03 +0300

    implement bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,37 @@
+// original implementation
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
+
+// my implementation (less efficient)
+// export default function bubble_sort(arr: number[]): void {
+//     let idx = 0;
+//     let lastIdx = arr.length;
+//
+//     while (lastIdx > 0) {
+//         const aIdx = idx;
+//         const bIdx = idx + 1;
+//         const a = arr[aIdx];
+//         const b = arr[bIdx];
+//
+//         if (a > b) {
+//             arr[aIdx] = b;
+//             arr[bIdx] = a;
+//         }
+//
+//         if (bIdx === lastIdx) {
+//             idx = 0;
+//             lastIdx -= 1;
+//         } else {
+//             idx += 1;
+//         }
+//     }
+// }
