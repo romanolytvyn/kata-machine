dc8bffe06a52aa11658b7b08ed4dcc6d574c3f94
Roman Lytvyn <lytvyn18@gmail.com>
2023-10-04 22:40:38 +0300

    impement Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,50 @@
+type Node<T> = {
+    value: T; next?: Node<T>;
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T> | undefined;
+    private tail?: Node<T> | undefined;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
+        this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        const node: Node<T> = {value: item};
+        this.length += 1;
 
-}
+        if (!this.tail) {
+            this.tail = node;
+            this.head = node;
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length -= 1;
+
+        const head = this.head;
+        this.head = head.next;
+
+        if (!this.length) {
+            this.tail = undefined;
+        }
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
