37fdfb29a13b60749133f15f545470c1c6e9acbb
Roman Lytvyn <lytvyn18@gmail.com>
2023-10-05 13:58:16 +0300

    implement Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,47 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+}
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        const node: Node<T> = {value: item};
 
-}
-    pop(): T | undefined {
+        this.length += 1;
 
-}
-    peek(): T | undefined {
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
 
-}
+        node.prev = this.head;
+        this.head = node;
+    }
+
+    pop() {
+        this.length = Math.max(0, this.length - 1);
+
+        if (this.length === 0) {
+            const head = this.head;
+            this.head = undefined;
+            return head?.value;
+        }
+
+        const head = this.head;
+        this.head = this.head?.prev;
+
+        return head?.value;
+    }
+
+    peek() {
+        return this.head?.value;
+    }
 }
